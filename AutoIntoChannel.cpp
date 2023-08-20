0c46f8623a37c7d876c0fe9320ddc9e91a526fb1
411956158 <Li20001128@163.com>
2023-08-20 22:27:41 +0800

    CODE upload
    上传了必要的代码

---
+++ AutoIntoChannel.cpp
@@ -0,0 +1,148 @@
+﻿// AutoIntoChannel.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+int openFile(std::string address);
+int simulateCtrlS();
+void printDashEverySecond(int ms100);
+HWND FindTeamSpeakWindow();
+
+int forceSucccess = 0;
+
+std::string address = "start C:\\Users\\Public\\Desktop\\TeamSpeak3Client.lnk";
+
+int main()
+{
+	/*printDashEverySecond();
+    std::cout << "Hello World!\n";
+	printDashEverySecond();
+	printf("等待安全时间\n");
+	printDashEverySecond();
+	printDashEverySecond();
+	// Wait for user to press any key
+	//std::cout << "Press any key to continue...\n";
+	//std::cin.get();*/
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);//设置浅黄色
+	printf("开始打开文件\n");
+	if (openFile(address)) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);//设置浅绿色
+		printf("开始打开文件成功\n\n");
+	}
+	else {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);//设置浅红色
+		printf("开始打开文件失败，请查看日志\n\n");
+	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);//设置浅黄色
+
+	printDashEverySecond(6);
+
+	FindTeamSpeakWindow();
+	if (forceSucccess == 0) {
+		printDashEverySecond(5);
+		FindTeamSpeakWindow();
+	}
+	if (forceSucccess == 1) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);//设置浅绿色
+		printf("赋予窗口焦点成功\n\n");
+	}
+	else {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);//设置浅红色
+		printf("赋予窗口焦点失败，请查看日志\n\n");
+	}
+
+	printDashEverySecond(2);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);//设置浅黄色
+	printf("开始连接服务器\n");
+	simulateCtrlS();
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);//设置浅绿色
+	printf("连接服务器成功\n");
+
+	exit(114514);
+}
+
+void printDashEverySecond(int ms100) {
+	for(int i=0;i<ms100;i++) {
+		std::cout << "-";
+		Sleep(500); // Sleep for 1 second
+	}
+	std::cout << "\n";
+}
+
+int simulateCtrlS() {
+	// This function simulates the pressing of Ctrl+S keys
+	// VK_CONTROL represents the 'Ctrl' key and 'S' represents the 'S' key
+	// KEYEVENTF_KEYUP is used to simulate the release of a key
+
+	// Press Ctrl key
+	keybd_event(VK_CONTROL, 0, 0, 0);
+
+	// Press S key
+	keybd_event('S', 0, 0, 0);
+
+	// Release S key
+	keybd_event('S', 0, KEYEVENTF_KEYUP, 0);
+
+	// Release Ctrl key
+	keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+
+	printDashEverySecond(5);
+
+	// Simulate pressing the 'Enter' key
+	keybd_event(VK_RETURN, 0, 0, 0);
+
+	// Release the 'Enter' key
+	keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+
+	return 1;
+}
+
+
+int openFile(std::string address) {
+	if (system(address.c_str()) != -1) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+HWND FindTeamSpeakWindow(){
+	HWND hwnd = NULL;
+	wchar_t className[512];
+	wchar_t windowName[512];
+
+	// Get the first top-level window
+	hwnd = GetTopWindow(NULL);
+
+	// Iterate through all top-level windows
+	while (hwnd != NULL)
+	{
+		// Get the class name of the window
+		GetClassNameW(hwnd, className, sizeof(className)/2);
+
+		// Get the window name of the window
+		GetWindowTextW(hwnd, windowName, sizeof(windowName)/2);
+		printf("%ws\n", windowName);
+
+		// Check if the window name contains "TeamSpeak"
+		if (wcsstr(windowName, L"TeamSpeak") != NULL)
+		{
+			// Set the focus to the window
+			//SetFocus(hwnd);
+			SetForegroundWindow(hwnd);
+			printf("AAA赋予窗口焦点成功%ws\n\n", windowName);
+			forceSucccess = 1;
+			//break;
+		}
+
+		// Get the next top-level window
+		hwnd = GetNextWindow(hwnd, GW_HWNDNEXT);
+	}
+
+	return hwnd;
+}
